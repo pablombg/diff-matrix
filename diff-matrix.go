9c7d33c9440598876dd1f1ab6da548d8606d6ce3
Pablo M. Bermudo Garay <pablombg@gmail.com>
2018-11-12 13:58:03 +0100

    Initial commit
    Just check and print the arguments.

---
+++ diff-matrix.go
@@ -0,0 +1,40 @@
+/*  This file is part of diff-matrix.
+ *
+ *  Copyright (C) 2018  Pablo M. Bermudo Garay
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for _, dir := range os.Args[1:] {
+		file, err := os.Stat(dir)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		} else if !file.Mode().IsDir() {
+			fmt.Printf("'%s' is not a directory\n", dir)
+			os.Exit(1)
+		}
+
+		fmt.Println(dir)
+	}
+}
